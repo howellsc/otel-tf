c35e39ea2ccd86334a1073849a94fcb0d7ab1dd1
chrish <chris.d.howells@gmail.com>
2025-10-01 12:12:14 +0100

    Initial Commit

--- resources/main.tf
+++ resources/main.tf
@@ -51,3 +51,19 @@ resource "google_compute_instance" "otel_collector" {
 
   tags = ["otel-collector"]
 }
+
+# Cloud Router
+resource "google_compute_router" "otel_nat_router" {
+  name    = "otel-nat-router"
+  region  = var.region
+  network = google_compute_network.otel_net.name
+}
+
+# Cloud NAT
+resource "google_compute_router_nat" "otel_cloud_nat" {
+  name                               = "otel-cloud-nat"
+  router                             = google_compute_router.otel_nat_router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
