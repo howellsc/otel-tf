8263435db28f5e40ab04d558be82eeeb9b1c00f1
chrish <chris.d.howells@gmail.com>
2025-10-01 12:57:31 +0100

    Initial Commit

--- resources/startup.sh
+++ resources/startup.sh
@@ -4,7 +4,6 @@ set -e
 # Update OS
 dnf update -y
 
-yum update
 yum -y install wget
 wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.136.0/otelcol_0.136.0_linux_amd64.rpm
 rpm -ivh otelcol_0.136.0_linux_amd64.rpm
