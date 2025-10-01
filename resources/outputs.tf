75d45cc2445b26b57941779c0d1f0ffb7ae06997
chrish <chris.d.howells@gmail.com>
2025-10-01 12:08:06 +0100

    Initial Commit

--- resources/outputs.tf
+++ resources/outputs.tf
@@ -1,4 +1,1 @@
-output "otel_collector_external_ip" {
-  value       = google_compute_instance.otel_collector.network_interface[0].access_config[0].nat_ip
-  description = "External IP address for OTEL collector (so your apps can push metrics/traces to it)"
-}
+
