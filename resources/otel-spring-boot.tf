86c274240efb372d52090dad3be8007929c8bb99
chrish <chris.d.howells@gmail.com>
2025-10-02 13:13:53 +0100

    Initial Commit

--- resources/otel-spring-boot.tf
+++ resources/otel-spring-boot.tf
@@ -5,7 +5,7 @@ resource "google_compute_instance" "ubuntu_vm" {
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      image = "rocky-linux-cloud/rocky-linux-9"
       size  = var.disk_size_gb
     }
   }
--- resources/startup-otel-spring-boot.sh
+++ resources/startup-otel-spring-boot.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
+set -e
 
-apt-get update
-apt-get install -y docker.io curl tar
+# Update OS
+dnf update -y
 
-systemctl start docker
+yum -y install podman
 
 # A name for the container and image
 CONTAINER_NAME="spring-boot-otel"
-IMAGE="europe-north1-docker.pkg.dev/ritm4225265/spring/otel-spring-boot"
+REGISTRY="europe-north1-docker.pkg.dev"
+IMAGE="ritm4225265/spring/otel-spring-boot"
 
-# Set home directory to save docker credentials
-export HOME=/home/appuser
-mkdir -p $HOME
+# Get access token from gcloud
+TOKEN=$(gcloud auth print-access-token)
 
-# Configure docker with credentials for gcr.io and pkg.dev
-VERSION=2.1.29
-OS=linux  # or "darwin" for OSX, "windows" for Windows.
-ARCH=amd64  # or "386" for 32-bit OSs
-
-curl -fsSL "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz" \
-| tar xz docker-credential-gcr \
-&& chmod +x docker-credential-gcr && sudo mv docker-credential-gcr /usr/bin/
-
-docker-credential-gcr configure-docker
+podman login -u oauth2accesstoken -p "$TOKEN" https://$REGISTRY
 
 # Stop and remove the container if it exists
-docker stop $CONTAINER_NAME || true
-docker rm $CONTAINER_NAME || true
+podman stop $CONTAINER_NAME || true
+podman rm $CONTAINER_NAME || true
 
 # Pull the latest version of the container image
-docker pull $IMAGE
+podman pull $REGISTRY/$IMAGE
 
 # Run docker container
-docker run -d --network=host --name $CONTAINER_NAME -p 5000:5000 $IMAGE
+podman run -d --network=host --name $CONTAINER_NAME -p 5000:5000 $IMAGE
