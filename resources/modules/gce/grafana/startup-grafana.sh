0cb06a74a6944a0dc16818c9e3015e741285ecde
chrish <chris.d.howells@gmail.com>
2025-10-08 10:41:39 +0100

    Initial Commit

--- resources/modules/gce/grafana/startup-grafana.sh
+++ resources/modules/gce/grafana/startup-grafana.sh
@@ -20,7 +20,7 @@ sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
 
 wget -nv https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.136.0/otelcolotelcol-contrib_0.136.0_linux_amd64.rpm
-rpm -ivh otelcol_0.136.0_linux_amd64.rpm
+rpm -ivh otelcol-contrib_0.136.0_linux_amd64.rpm
 
 cat > /etc/otelcol/config.yaml <<EOF
 receivers:
